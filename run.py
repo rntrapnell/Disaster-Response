0df15d88cff1c6a7f6e3a61a56b8174c4d49460e
Richard Trapnell <rntrapnell@gmail.com>
2019-04-04 15:53:53 -0500

    change file paths in run.py

--- run.py
+++ run.py
@@ -26,27 +26,27 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../workspace/data/DisasterResponse.db')
+engine = create_engine('sqlite:///DisasterResponse.db')
 df = pd.read_sql_table('table1', engine)
 
 # load model
-model = joblib.load("../workspace/models/classifier.pkl")
+model = joblib.load("classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
+
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
     categories = ['related', 'request', 'offer', 'aid_related', 'medical_help', 'medical_products', 'search_and_rescue', 'security', 'military', 'child_alone', 'water', 'food', 'shelter', 'clothing', 'money', 'missing_people', 'refugees', 'death', 'other_aid', 'infrastructure_related', 'transport', 'buildings', 'electricity', 'tools', 'hospitals', 'shops', 'aid_centers', 'other_infrastructure', 'weather_related', 'floods', 'storm', 'fire', 'earthquake', 'cold', 'other_weather', 'direct_report']
     category_totals = df[categories].sum()
     category_names = df[categories].columns
-    
+
     categories_checked = df[categories].sum(axis =1)
     categories_count = categories_checked.value_counts()
-    
+
     cat_x = categories_count.index
     cat_y = categories_count.values
     print (cat_x)
@@ -90,11 +90,11 @@ def index():
             }
         }
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -103,13 +103,13 @@ def index():
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html Please see that file.
     return render_template(
         'go.html',
         query=query,
